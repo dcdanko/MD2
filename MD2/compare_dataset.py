3a522a79ddee89be000018758f7ca9ef9b0fc7dc
David C Danko <dcdanko@gmail.com>
2019-07-09 14:30:39 -0400

    detailed scaffold for Issue #5

--- MD2/compare_dataset.py
+++ MD2/compare_dataset.py
@@ -2,13 +2,32 @@ import pandas as pd
 import numpy as np
 from scipy.stats import chisquare
 
-def compare_datasets():
-#import two files from command line
-#parse data into dataframe
-#work with data 
+
+def compare_datasets(taxa_list_1, taxa_list_2):
+    """Return a Pandas DataFrame listing differences between two taxa lists.
+    
+    The DataFrame should have the following columns:
+     1. variable, the thing being compared (e.g. spore forming)
+     2. dataset, which dataset is being compared (e.g. taxa_list_1)
+     3. value, the value being listed. For categorical variables this
+        will be a value of the categorical variable (e.g. red from 
+        [red, green, blue]). For numeric variables this will be the 
+        name of a summary statistic (e.g. mean, median, mode)
+     4. abundance-in, the value of (3) in (2) (e.g. taxa list 1)
+     5. abundance-out, the value of (3) not in (2) (e.g. taxa list 2)
+     6. p-value, the p-value of the difference
+     
+    This is a long format dataframe which means some of the data may
+    be repeated.
+    """
     pass
 
-def statistical_testing():
-#try statistical test
-#return p-value
+
+def compare_categorical(value_being_compared, values_in_taxa_list_1, values_in_taxa_list_2):
+    """Return a Pandas Series with [abundance-in, abundance-out, p-value]."""
+    pass
+
+
+def compare_numeric(values_in_taxa_list_1, values_in_taxa_list_2):
+    """Return a Pandas Series with [abundance-in, abundance-out, p-value]."""
     pass
