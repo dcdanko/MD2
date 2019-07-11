cdc732af3b1bec3d8266011d1d158bc4f8d568c1
Chandrima Bhattacharya <chandrima.004@gmail.com>
2019-07-11 14:24:08 -0400

    Return rank and taxonomic id

--- MD2/cli.py
+++ MD2/cli.py
@@ -9,6 +9,7 @@ def main():
 	
 @main.command('data-table')
 def data_table():
+    """Merge existing data tables"""
     pass
 	
 @main.command('filter-microbes')
@@ -26,16 +27,21 @@ def filter_taxa(out):
     annotated.to_csv(out)
 	
 @main.command('annotate-taxa')
+@click.option('--microbes', default=True, help='Whether limit to microbes i.e., Bacteria and Virus, only')
 @click.argument('out', type=click.File('w'))
-def annotate(out):
-    """Parse NCBI File to return the phyla classification of a species"""
+def annotate(microbes, out):
+    """Parse NCBI File to return the rank for given scientific name"""
     taxa_tree, sci_name = NCBITaxaTree.parse_files()
-    phyla = list()
+    rank_file = {}
     for taxon in sci_name:
-        taxon = taxon.strip()  
-        phyla.append([taxa_tree.phyla(taxon, default='unknown')])
-    annotated = pd.DataFrame.from_dict({'taxa': sci_name, 'phyla': phyla}, orient='columns')
-    annotated = annotated.set_index('taxa')
+        taxon = taxon.strip()
+        if microbes == False:        
+            rank_file[taxon] = taxa_tree.rank_of_species(taxon).values()
+        else:
+            if taxa_tree.rank_microbes(taxon, default=None) != None:
+                rank_file[taxon] = taxa_tree.rank_microbes(taxon, default=None).values()
+    col_names = ['scientific name', 'taxonomic_id', 'rank']
+    annotated = pd.DataFrame.from_dict(rank_file, columns=col_names, orient='index')
     annotated.to_csv(out)
 	
 
