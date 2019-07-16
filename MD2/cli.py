d8b799820ad2045a860b130459c53e6e94e3bd81
Chandrima Bhattacharya <chandrima.004@gmail.com>
2019-07-15 20:35:45 -0400

    Code added to merge with all datasets
    1. All dataset must be in the folder and folder path should be provided
    2. all dataset should be CSV

--- MD2/cli.py
+++ MD2/cli.py
@@ -1,6 +1,5 @@
 import click
 import pandas as pd
-import glob
 import csv
 from .taxa_tree import NCBITaxaTree
 
@@ -8,22 +7,6 @@ from .taxa_tree import NCBITaxaTree
 @click.group()
 def main():
     pass
-	
-@main.command('data-table')
-@click.argument('file_path', type=click.Path(exists=True))
-@click.argument('out', type=click.File('w'))
-def data_table(file_path, out):
-    """Concanate all the CSV files into one combined file"""
-    all_files = glob.glob(file_path + "/*.csv")
-    concat_list = []
-    
-    for filename in all_files: 
-        df = pd.read_csv(open(filename, 'r'))
-        concat_list.append(df)
-
-    annotated = pd.concat(concat_list, axis=0, ignore_index=True)
-    annotated.to_csv(out)
-
 	
 @main.command('filter-microbes')
 @click.argument('out', type=click.File('w'))
@@ -41,20 +24,20 @@ def filter_taxa(out):
 	
 @main.command('annotate-taxa')
 @click.option('--microbes', default=True, help='Whether limit to microbes i.e., Bacteria and Virus, only')
-@click.argument('out', type=click.File('w'))
-def annotate(microbes, out):
-    """Parse NCBI File to return the rank for given scientific name"""
+@click.argument('file_path', type=click.Path(exists=True))
+def annotate(microbes, file_path):
+    """Parse NCBI File to return the rank for given scientific name along with merged tables for all datasets"""
     taxa_tree, sci_name = NCBITaxaTree.parse_files()
     rank_file, tax_rank, rank = {}, list(), ''
     bacteria, viruses, fungi = {}, {}, {}
+    merged_file = taxa_tree.data_table(file_path)
     for taxon in sci_name:
         taxon = taxon.strip()
         if microbes == False:        
             rank_file[taxon] = taxa_tree.rank_of_species(taxon).values()
         else:
             tax_rank, rank = taxa_tree.rank_microbes(taxon, default=None)
-            rank_file[taxon] = tax_rank
-            if rank != None:   
+            if rank != None: 		
                 if rank == 'Viruses':
                     viruses[taxon] = tax_rank
                 elif rank == 'Bacteria':
@@ -62,13 +45,15 @@ def annotate(microbes, out):
                 elif rank == 'Fungi':
                     fungi[taxon] = tax_rank			
     col_names = ['scientific name', 'taxonomic_id', 'rank']
-    annotated = pd.DataFrame.from_dict(rank_file, columns=col_names, orient='index')
-    annotated.to_csv(out)
-    annotated = pd.DataFrame.from_dict(viruses, columns=col_names, orient='index')
-    annotated.to_csv("NCBI_Virus_rank.csv")
     annotated = pd.DataFrame.from_dict(bacteria, columns=col_names, orient='index')
+    annotated = annotated.set_index('scientific name')
+    annotated = annotated.join(merged_file, how='left')
     annotated.to_csv("NCBI_Bacteria_rank.csv")
+    annotated = pd.DataFrame.from_dict(viruses, columns=col_names, orient='index')
+    annotated = annotated.join(merged_file, how='left')
+    annotated.to_csv("NCBI_Virus_rank.csv")
     annotated = pd.DataFrame.from_dict(fungi, columns=col_names, orient='index')
+    annotated = annotated.join(merged_file, how='left')
     annotated.to_csv("NCBI_Fungi_rank.csv")
 	
 
