bfef87c49407275c14c2b306855aa31f9facc8ec
Sophie-02 <52670573+Sophie-02@users.noreply.github.com>
2019-08-22 11:52:38 -0400

    Update weight_comparisons.py

--- MD2/weight_comparisons.py
+++ MD2/weight_comparisons.py
@@ -69,3 +69,37 @@ if __name__ == '__main__':
             {4:0.2, 6:0.125, 7:0.3, 8:0.375},
     )
     print(numeric_abundances_test)
+
+    def compare_microbe_directory_dataframes(values_in_taxa_list_1, values_in_taxa_list_2):
+    df_final = pd.DataFrame(columns = ['variable', 'type', 'dataset', 'value', 'abundance_in', 'abundance_out', 'p-value'])
+    for column_name in CATEGORICAL_LIST:
+        taxa_list1 = values_in_taxa_list_1[column_name]
+        taxa_list2 = values_in_taxa_list_2[column_name]
+        values_list = (values_in_taxa_list_1[column_name] + values_in_taxa_list_1[column_name]).unique()
+        for var in values_list:
+            categorical = compare_categorical(var, values_in_taxa_list_1[column_name], values_in_taxa_list_2[column_name])
+            df_final = df_final.append({'variable': column_name, 'type': 'categorical', 'dataset': 'df', 'value': var, 'abundance_in': categorical[0], 'abundance_out': categorical[1], 'p-value': categorical[2]}, ignore_index = True)
+    for column_name in NUMERICAL_LIST:
+        numeric = compare_numeric(values_in_taxa_list_1[column_name], values_in_taxa_list_2[column_name])
+        df_final = df_final.append({'variable': column_name, 'type': 'numerical', 'dataset': 'df', 'value': 'mean', 'abundance_in': numeric[0], 'abundance_out': numeric[1], 'p-value': numeric[2]}, ignore_index = True)
+    return df_final
+
+
+def compare_taxa_lists(values_in_taxa_list_1, values_in_taxa_list_2):
+    """Return a Pandas DataFrame listing differences between two taxa lists.
+    The DataFrame should have the following columns:
+     1. variable, the thing being compared (e.g. spore forming)
+     2. dataset, which dataset is being compared (e.g. taxa_list_1)
+     3. value, the value being listed. For categorical variables this
+        will be a value of the categorical variable (e.g. red from
+        [red, green, blue]). For numeric variables this will be the
+        name of a summary statistic (e.g. mean, median, mode)
+     4. abundance-in, the value of (3) in (2) (e.g. taxa list 1)
+     5. abundance-out, the value of (3) not in (2) (e.g. taxa list 2)
+     6. p-value, the p-value of the difference
+    This is a long format dataframe which means some of the data may
+    be repeated.
+    """
+    df1 = MICROBE_DIRECTORY.loc[values_in_taxa_list_1]
+    df2 = MICROBE_DIRECTORY.loc[values_in_taxa_list_2]
+    return compare_microbe_directory_dataframes(df1, df2)
