ea8cd658876c81a41f1b0766c5bd52fc3746da5d
Chandrima Bhattacharya <chandrima.004@gmail.com>
2019-08-23 02:07:02 -0400

    New methods added

--- MD2/cli.py
+++ MD2/cli.py
@@ -14,6 +14,10 @@ from .dataset_modification import (
     metasub_process,
     convert_taxa_tree,
 	)
+from .dataset_stats import (
+    verify_column_names,
+    dataset_stats
+    )
 	
 @click.group()
 def main():
@@ -34,53 +38,43 @@ def filter_taxa(out):
     annotated.to_csv(out)
 	
 @main.command('annotate-taxa')
-@click.option('--microbes', default=True, help='Whether limit to microbes i.e., Bacteria and Virus, only')
 @click.argument('file_path', type=click.Path(exists=True))
-def annotate(microbes, file_path):
+def annotate(file_path):
     """Parse NCBI File to return the rank for given scientific name along with merged tables for all datasets"""
     taxa_tree, sci_name = NCBITaxaTree.parse_files()
     rank_file, tax_rank, rank = {}, list(), ''
     bacteria, viruses, fungi = {}, {}, {}
     for taxon in sci_name:
         taxon = taxon.strip()
-        if microbes == False:        
-            rank_file[taxon] = taxa_tree.rank_of_species(taxon).values()
-        else:
-            tax_rank, rank = taxa_tree.rank_microbes(taxon, default=None)
-            if rank != None: 		
-                if rank == 'Viruses':
-                    viruses[taxon] = tax_rank
-                elif rank == 'Bacteria':
-                    bacteria[taxon] = tax_rank					
-                elif rank == 'Fungi':
-                    fungi[taxon] = tax_rank							
-    col_names = ['scientific name', 'taxonomic_id', 'rank']
+        tax_rank, rank = taxa_tree.rank_microbes(taxon, default=None)
+        if rank != None: 		
+            if rank == 'Viruses':
+                viruses[taxon] = tax_rank
+            elif rank == 'Bacteria':
+                bacteria[taxon] = tax_rank
+            elif rank == 'Fungi':
+                fungi[taxon] = tax_rank	
+    col_names = ['scientific_name', 'taxonomic_id', 'rank']
     annotated = pd.DataFrame.from_dict(viruses, columns=col_names, orient='index')
-    annotated = taxa_tree.data_table(file_path, annotated)
-    annotated.to_csv("NCBI_Virus_rank.csv")
+    annotated = taxa_tree.data_table(file_path, annotated)					
+    cleaned_file = clean_file(True, annotated)	
+    final_file, statistics = verify_column_names(cleaned_file, 'virus')
+    final_file.to_csv("NCBI_Virus_rank.csv")	
+    statistics.to_csv("Virus_statistics.csv")	       
     annotated = pd.DataFrame.from_dict(bacteria, columns=col_names, orient='index')
-    annotate = taxa_tree.data_table(file_path, annotated)
-    annotate.to_csv("NCBI_Bacteria_rank.csv")
+    annotated = taxa_tree.data_table(file_path, annotated)		
+    add_new_datas = filter_taxa(annotated)			
+    cleaned_file = clean_file(False, add_new_datas)	
+    final_file, statistics = verify_column_names(cleaned_file, 'bacteria')
+    final_file.to_csv("NCBI_Bacteria_rank.csv")	
+    statistics.to_csv("Bacteria_statistics.csv")					       
     annotated = pd.DataFrame.from_dict(fungi, columns=col_names, orient='index')
     annotated = taxa_tree.data_table(file_path, annotated)
-    annotated.to_csv("NCBI_Fungi_rank.csv")
-	
-@main.command('clean-file')
-@click.option('--isvirus', default=False, help='Whether the file contains virus')
-@click.argument('file', type=click.File('r'))
-@click.argument('out', type=click.File('w'))
-def clean_file(isvirus, file, out):
-    """Clean up data-table to be used for Microbe Directory 2.0 and above"""
-    file_name = pd.read_csv(file, index_col=False)
-    header = list(file_name.columns.values)
-    remove_col_file = reduce_col(isvirus, file_name)
-    if isvirus == False:
-	    add_values = filter_taxa(remove_col_file)
-        remove_row_file = reduce_row(isvirus, add_values)
-    else:
-        remove_row_file = reduce_row(isvirus, remove_col_file) 
-    remove_row_file = remove_row_file.replace('nan', '')
-    remove_row_file.to_csv(out)
+    cleaned_file = clean_file(False, annotated)	
+    final_file, statistics = verify_column_names(cleaned_file, 'fungi')
+    final_file.to_csv("NCBI_Fungi_rank.csv")	
+    statistics.to_csv("Fungi_statistics.csv")
+                    
 	
 @main.command('metasub-preprocessing')
 @click.option('--feature-name', default='city', help='The feature to consider')
@@ -106,17 +100,35 @@ def dataset_preprocess(feature_name, subtext, file, biom_file, metadata_file, ou
     compiled_metasub = convert_taxa_tree(file, biom_file, metadata_file, feature_name, subtext)
     compiled_metasub.to_csv(out)
 	
+def clean_file(isvirus, file_name):
+    """Clean up data-table to be used for Microbe Directory 2.0 and above"""
+    header = list(file_name.columns.values)
+    remove_col_file = reduce_col(isvirus, file_name)
+    remove_row_file = reduce_row(isvirus, remove_col_file) 
+    remove_row_file = remove_row_file.replace('nan', '')
+    return remove_col_file
+	
 def filter_taxa(file_name):
     """Parse NCBI File to fill a column which exhibits certain hierarchical traits"""
     taxa_tree, sci_name = NCBITaxaTree.parse_files()
     rank_list = ['subspecies', 'species', 'species group', 'species subgroup', 'subgenus', 'genus', 'subfamily', 'family', 'suborder', 'order', 
 		        'subclass', 'class', 'subphylum', 'phylum', 'kingdom', 'superkingdom', 'no rank', 'varietas', 'forma', 'tribe']
-    spore_forming_genus = ['Bacillus', 'Clostridium', 'Sporolactobacillus', 'Sporosarcina']		
+    spore_forming_genus = ['Bacillus', 'Clostridium', 'Sporolactobacillus', 'Sporosarcina', 'Cerasibacillus', 'Alkalibacillus', 'Amphibacillus',
+                            'Anoxybacillus', 'Filobacillus', 'Geobacillus', 'Gracilibacillus', 'Halobacillus', 'Lentibacillus', 'Oceanobacillus',
+                            'Paraliobacillus', 'Pontibacillus', 'Tenuibacillus', 'Thalassobacillus', 'Virgibacillus', 'Jeotgalibacillus', 
+                            'Marinibacillus', 'Planomicrobium', 'Ureibacillus', 'Sporobacterium', 'Desulfitobacterium', 'Desulfonispora', 
+                            'Desulfosporosinus', 'Desulfotomaculum', 'Sporotomaculum', 'Syntrophobotulus', 'Pelotomaculum', 'Sporotomaculum', 
+                            'Syntrophobotulus', 'Thermincola', 'Filifactor', 'Tepidibacter', 'Anaerotruncus', 'Oscillospira', 'Sporobacter']		
+    not_spore_forming_genus = ['Halolactibacillus', 'Marinococcus', 'Saccharococcus', 'Planococcus', 'Caryophanon', 'Filibacter', 'Kurthia',
+                                'Peptococcus', 'Cryptanaerobacter', 'Dehalobacter', 'Anaerofilum', 'Acetivibrio', 'Acetanaerobacterium', 
+                                'Fastidiosipila', 'Papillibacter', 'Subdoligranulum']
     for index, rows in file_name.iterrows():
         #Update Spore_Forming in a Top-Down approach based on Genus
         if rank_list.index(rows['rank']) <= rank_list.index('genus'):
             if taxa_tree.genus(rows['scientific_name'], default=None) in (spore_forming_genus):
                 file_name.loc[file_name['scientific_name']==rows['scientific_name'], 'spore_forming'] = 'Always'
+            elif taxa_tree.genus(rows['scientific_name'], default=None) in (not_spore_forming_genus):
+                file_name.loc[file_name['scientific_name']==rows['scientific_name'], 'spore_forming'] = 'Never'
         #Update Gram_Stain in a Mixed approach based on Genus
         if taxa_tree.ancestors_list('genus', rows['scientific_name'], default=None) != None:
             ancestors = taxa_tree.ancestors_list('genus', rows['scientific_name'], default=None).copy()
@@ -126,7 +138,7 @@ def filter_taxa(file_name):
             elif 'Negative'  in df_val['gram_stain'].values:
                 file_name.loc[file_name['scientific_name'].isin(ancestors), 'gram_stain'] = 'Negative'           
     return file_name
-    
+	
 
 if __name__ == '__main__':
     main()
