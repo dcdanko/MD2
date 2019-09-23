22e8f005c83fb2c099be2d9d501540cf7a4357c1
Chandrima Bhattacharya <chandrima.004@gmail.com>
2019-09-23 13:26:25 -0400

    Update clean_table.py

--- microbe_directory/clean_table.py
+++ microbe_directory/clean_table.py
@@ -6,7 +6,6 @@ REGEX_COLUMN = [
     'genus', 
     'family', 
     'order', 
-    'class', 
     'phylum', 
     'domain', 
     'kingdom', 
@@ -17,6 +16,9 @@ REGEX_COLUMN = [
     'microbe_id',
     'otu',
     ' id',
+    'non_null',
+    'pmid',
+    'sample type', 
     ]
 	
 REGEX_COUNT_COL = [
@@ -25,101 +27,27 @@ REGEX_COUNT_COL = [
     '_id_',
     ]
 	
-REGEX_TAXANOMY = [
-    'eubacterium',
-    'bacterium ',
-    'sp.',
-    'phytoplasma',
-    'uncultured',
-    'unidentified',
-    'symbiont',
-    'killing',
-    'containing',
-    'primary',
-    'secondary',
-    'alpha',
-    'beta',
-    'gamma',
-    'delta',
-    'endosymbiont',
-    'adapted',
-    'aerobic',
-    'methanotroph',
-    'clinical sample',
-    'isolate',
-    'ice',
-    'sterile',
-    'pathogen',
-    'clone',
-    'like',
-    'degrading',
-    'bacerium',
-    'associated',
-    'vouchered',
-    'fungal endophyte',
-    'cf.',
-    's.l.',
-    'sect.',
-    'aff.',
-    'strain',
-    '16Sr',
-	'str.',
-    'culture',
-    'enrichment',
-    'human',
-    'diazotroph',
-    ' of ',
-    'et al.',
-    'planctomycete ',
-    ' bacterium',
-    'phytoplasma',
-    'obligately',
-    'soil',
-    'marine',
-    'candidate',
-    'endo',
-    'exo',
-    'complex',
-    'type',
-    'mycorrhiza',
-    'root',
-    'tip',
-    'rot',
-    '\'',
-    '\[',
-    '\('
-    ]
 
-def reduce_col(isvirus, file):
+def file_clean(file):
+    column_clean = reduce_col(file)
+    file_renamed = modify_dataset_value(column_clean)	
+    new_file = file_renamed.groupby(['scientific_name', 'taxonomic_id', 'rank'], as_index=True).agg(lambda x: ( ', '.join( repr(e) for e in list(set(x)))))
+    return new_file.replace('nan, ', '', regex=True).replace('\'', '').replace('[', '').replace(']', '').applymap(lambda x: x.replace('\'', ''))
+	
+	
+def reduce_col(file):
     """Remove empty columns, ids and taxonomy columns"""
     drop_col = file.dropna(axis='columns', how='all')
     drop_col.columns = map(str.lower, drop_col.columns)
-    col_names = ['class']
+    col_names = list()
     for reg in REGEX_COLUMN:    
         col_names.extend(list(drop_col.filter(regex=reg)))
-    #if isvirus == 'True': 
-    #    col_names.remove('id')
-    drop_col = drop_col.drop(columns=col_names, axis=1)
+    drop_col = drop_col.drop(columns=col_names, axis=0)
     drop_col.columns = rename_col(drop_col)	
     final_file = rename_MD1_tables(drop_col)
     return final_file
 
-def reduce_row(isvirus, file):
-    """Remove duplicate rows and subspecies"""
-    if isvirus == 'True': 
-        REGEX_TAXANOMY.remove('bacterium ')
-        REGEX_TAXANOMY.remove('human')
-    for reg in REGEX_TAXANOMY:
-        non_null = file.count(axis = 1) 
-        filter = file['scientific_name'].str.contains(reg)
-        for index, values in filter.items():
-            if values == True and non_null[index] > 3:
-                filter[index] = False
-        file = file[~filter]
-    file_renamed = modify_dataset_value(file)	
-    new_file = file_renamed.groupby(['scientific_name', 'taxonomic_id', 'rank'], as_index=True).agg(lambda x: ( ', '.join( repr(e) for e in list(set(x)))))
-    return new_file.replace('nan, ', '', regex=True).replace('\'', '').replace('[', '').replace(']', '').applymap(lambda x: x.replace('\'', ''))
-	
+
 def rename_col(file):
     """Convert column names as per snakelowercase standards"""
     file.columns = file.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('_x', '_').str.replace('_y', '_').str.replace('.', '_').str.replace(',', '_')
@@ -160,6 +88,3 @@ def clean_count_datasets(file, regex_list):
         file[reg] = file[reg].mask((file[reg]>=75) & (file[reg]<100), 4)
         file[reg] = file[reg].replace([0, 2, 3, 4, 100], ['Never Observed', 'Rarely Observed', 'Fairly Observed', 'Mostly Observed', 'Always Observed'])
     return file
-
-
-
