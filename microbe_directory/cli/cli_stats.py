cc47e1b4d7627b41245a56a3b156432f0736a44c
David Danko <dcdanko@gmail.com>
2019-11-26 19:38:55 -0500

    Updated stats function

--- microbe_directory/cli/cli_stats.py
+++ microbe_directory/cli/cli_stats.py
@@ -15,29 +15,18 @@ def stats():
     pass
 
 
-@stats.command('stats-file')
-@click.option('--microbe', default='bacteria', type=click.Choice(DOMAINS),
-              help='Biological domain of the table')
+@stats.command('table')
+@click.option('-s', '--sep', default='\t')
 @click.option('-o', '--outfile', default='-', type=click.File('w'))
 @click.argument('table', type=click.File('r'))
-def clean_file(microbe, outfile, table):
-    """Outputs the statistics of column that needs to be filled."""
-    file_name = pd.read_csv(table)
-    final_file, stats_file = verify_column_names(file_name, microbe, default=None)
-    final_file.to_csv(outfile)
-    stats_file.to_csv(str(microbe + '_stats.csv'))
-
-
-@stats.command('column-compare')
-@click.option('-o', '--outfile', default='-', type=click.File('w'))
-@click.argument('table_1', type=click.File('r'))
-@click.argument('table_2', type=click.File('r'))
-def dataset_column_compare(outfile, table_1, table_2):
-    """Compare the before and after statistics of a file."""
-    table_1 = pd.read_csv(table_1)
-    table_2 = pd.read_csv(table_2)
-    table_1 = table_1.replace(r'^\s*$', np.nan, regex=True)
-    table_2 = table_2.replace(r'^\s*$', np.nan, regex=True)
-    stats1, stats2 = column_compare(table_1, table_2)
-    stats = pd.concat([stats1, stats2], sort=False, names=['title', 'before', 'after'], axis=1)
-    stats.to_csv(outfile)
+def cli_md2_table_stats(sep, outfile, table):
+    """Collate some statistics for a microbe directory table."""
+    tbl = pd.read_csv(table, index_col=0)
+    pf = lambda key, val: print(f'{key}{sep}{val}', file=outfile)
+    pf(f'Number of Taxa', tbl.shape[0])
+    pf(f'Number of columns', tbl.shape[1])
+    pf(f'Columns', ','.join(tbl.columns))
+    for col_name in tbl.columns:
+        col = tbl[col_name]
+        pf(f'{col_name} Filled', tbl.shape[0] - sum(col.isna()))
+        pf(f'{col_name} Unique Vals', len(col.unique()))
