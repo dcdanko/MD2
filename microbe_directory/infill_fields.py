33fbb33fce2081ee597fb801431c79acb47db128
David Danko <dcdanko@gmail.com>
2019-11-27 11:50:54 -0500

    vectorized version of bacterial infill

--- microbe_directory/infill_fields.py
+++ microbe_directory/infill_fields.py
@@ -11,43 +11,6 @@ from microbe_directory.constants import (
 )
 
 
-def spore_forming(scientific_name):
-    genus = taxa_tree.genus(scientific_name, default=None)
-    if genus:
-        if genus in SPORE_FORMING_GENERA:
-            return 'Always'
-        elif genus in NON_SPORE_FORMING_GENERA:
-            return 'Never'
-    return None
-
-
-def gram_stain(scientific_name):
-    try:
-        rank = taxa_tree.rank(scientific_name)
-    except KeyError:
-        return None
-    phylum = taxa_tree.phylum(scientific_name, default=None)
-    try:
-        return PHYLUM_GRAM_STAINS[phylum]
-    except KeyError:
-        pass
-    return None
-
-
-def infill_emp_col_func(col):
-    def infill_emp_col(scientific_name):
-        genus = taxa_tree.genus(scientific_name, default=None)
-        if genus:
-            try:
-                genus_value = col[genus]
-                if genus_value and str(genus_value).lower() not in ['nan', 'na', 'n/a', '']:
-                    return f'{genus_value} in Genus'
-            except KeyError:
-                pass
-        return None
-    return infill_emp_col
-
-
 def infill_bacterial_fields(table):
     """Return a copy of table with certain fields filled in.
 
@@ -56,16 +19,45 @@ def infill_bacterial_fields(table):
      - Gram stain based on stain of ancestor
      - Presence of relevant genus in the EMP
     """
-    table = table.copy(deep=True)
-    emp_cols = [
-        col for col in table.columns
-        if col.startswith('emp') or col.startswith('count')
-    ]
     taxa_tree = NCBITaxaTree.parse_files()
+
+    def spore_forming(scientific_name):
+        genus = taxa_tree.genus(scientific_name, default=None)
+        if genus:
+            if genus in SPORE_FORMING_GENERA:
+                return 'Always'
+            elif genus in NON_SPORE_FORMING_GENERA:
+                return 'Never'
+        return None
+
+    def gram_stain(scientific_name):
+        try:
+            rank = taxa_tree.rank(scientific_name)
+        except KeyError:
+            return None
+        phylum = taxa_tree.phylum(scientific_name, default=None)
+        try:
+            return PHYLUM_GRAM_STAINS[phylum]
+        except KeyError:
+            pass
+        return None
+
+    def infill_emp_col_func(col):
+        def infill_emp_col(scientific_name):
+            genus = taxa_tree.genus(scientific_name, default=None)
+            if genus:
+                try:
+                    genus_value = col[genus]
+                    if genus_value and str(genus_value).lower() not in ['nan', 'na', 'n/a', '']:
+                        return f'{genus_value} in Genus'
+                except KeyError:
+                    pass
+            return None
+        return infill_emp_col
 
     table['spore_forming'] = table['spore_forming'].fillna(table.index.map(spore_forming))
     table['gram_stain'] = table['gram_stain'].fillna(table.index.map(gram_stain))
-    for col in emp_cols:
+    for col in [col for col in table.columns if col.startswith('emp') or col.startswith('count')]:
         filler_func = infill_emp_col_func(col)
         table[col] = table[col].fillna(table.index.map(filler_func))
 
